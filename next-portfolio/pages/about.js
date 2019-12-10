e32c4b7aaa400b3681084bfa0ca33bc5054ced6e
unknown <ivan.mitkov@gmail.com>
2019-12-10 19:50:27 +0200

    fetching data server side

--- next-portfolio/pages/about.js
+++ next-portfolio/pages/about.js
@@ -1,22 +1,39 @@
 import Layout from "../components/Layout";
+import React from "react";
+import fetch from 'isomorphic-unfetch';
 
-const About = () => {
-  const styling = {
-    backgroundColor: "red",
-    backgroundImage: "url('/static/js-logo.png')",
-    width: "300px",
-    height: "300px"
-  };
-  return (
-    <Layout title="About">
-      <div>
-        <p>Something about me</p>
-        <img src="static/js-logo.png" alt="Javascript Logo" height="200px" />
+class About extends React.Component {
+  // state = {
+  //   user: null
+  // };
+  //run on client
+  // componentDidMount() {
+  //   fetch('https://api.github.com/users/Ivan-Mitkov').then(res=>res.json()).then(data=>{
+  //   this.setState({user:data})
+  //   })
+  // }
 
-        <div style={styling}> Javascript</div>
-      </div>
-    </Layout>
-  );
-};
+  
+  //run on server
+   static async getInitialProps() {
+    const res = await fetch("https://api.github.com/users/Ivan-Mitkov")
+     const data =await res.json()
+      return {user:data}
+  }
+
+  render() {
+   const {user}=this.props;
+    return (
+      <Layout title="About">
+        <div>
+    <p>{user.name}</p>
+          <img src={user.avatar_url} alt="Ivan" height="200px" />
+        </div>
+        {/* <div>{JSON.stringify(this.state.user)}</div> */}
+   
+      </Layout>
+    );
+  }
+}
 
 export default About;
