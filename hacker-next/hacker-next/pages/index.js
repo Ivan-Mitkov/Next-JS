a004d532d8defc686c6bd7cd757937c9ec608314
unknown <ivan.mitkov@gmail.com>
2019-12-12 07:06:33 +0200

    hacker news clone

---
+++ hacker-next/hacker-next/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div>Hacker Next</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
