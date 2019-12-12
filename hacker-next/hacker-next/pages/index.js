7236443fd2ee8cc1795bf597dd023ff5e4374405
unknown <ivan.mitkov@gmail.com>
2019-12-12 08:29:00 +0200

    get news

--- hacker-next/hacker-next/pages/index.js
+++ hacker-next/hacker-next/pages/index.js
@@ -1,7 +1,28 @@
 import Head from "next/head";
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+import StoryList from '../components/StoryList';
 
 class Home extends React.Component {
+  static async getInitialProps() {
+    let stories = null;
+    try {
+      const response = await fetch(
+        "https://node-hnapi.herokuapp.com/news?page=1"
+      );
+      stories = await response.json();
+    } catch (e) {
+      console.log(e);
+      stories = [];
+    }
+    return { stories };
+  }
+
   render() {
+    const { stories } = this.props;
+    if (stories.length === 0) {
+      return <Error statusCode={503} />;
+    }
     return (
       <div>
         <Head>
@@ -9,7 +30,8 @@ class Home extends React.Component {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <div>Hacker Next</div>
+        <h1>Hacker Next</h1>
+       <StoryList stories={stories}/>
       </div>
     );
   }
