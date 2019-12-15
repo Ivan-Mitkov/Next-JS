71e0d185cefa2deec6b5d312b894b4b2feab9e69
Ivan Mitkov <ivan.mitkov@gmail.com>
2019-12-15 15:49:20 +0200

    using next/router to create back button

--- hacker-next/hacker-next/components/Layout.js
+++ hacker-next/hacker-next/components/Layout.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Head from "next/head";
+import Router from "next/router";
 
-const Layout = ({ children, title, description }) => {
+const Layout = ({ children, title, description, backButton }) => {
   return (
     <div>
       <Head>
@@ -10,6 +11,12 @@ const Layout = ({ children, title, description }) => {
       </Head>
       <div className="container">
         <nav>
+          {backButton && (
+            // back button with Router
+            <span onClick={() => Router.back()} className="back-button">
+              &#x2b05;
+            </span>
+          )}
           <Link href="/">
             <a>
               <span className="main-title">Hacker Next</span>
@@ -37,6 +44,11 @@ const Layout = ({ children, title, description }) => {
           }
           nav .main-title {
             font-weight: bold;
+          }
+          nav .back-button {
+            font-size: 1.2rem;
+            padding-right: 1rem;
+            cursor:pointer;
           }
         `}
       </style>
