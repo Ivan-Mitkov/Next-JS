92647dbaa36f4fe65a07207bd61ec9e407c54e52
Ivan Mitkov <ivan.mitkov@gmail.com>
2019-12-29 12:01:13 +0200

    getRequestHandler() and nodemon

--- next-cookie-auth/server.js
+++ next-cookie-auth/server.js
@@ -5,8 +5,11 @@ const dev =process.env.NODE_ENV!=='production';
 const port =process.env.port||3000;
 const app=next({dev});
 
+const handler=app.getRequestHandler()
+
 app.prepare().then(()=>{
     const server=express();
+    server.get('*',(req,res)=>handler(req,res))
     server.listen(port,err=>{
         if(err){
             console.log(err.message)
