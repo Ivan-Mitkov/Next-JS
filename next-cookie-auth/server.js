a96594beb5217265a400cc8a6cb8d45809ad2a78
Ivan Mitkov <ivan.mitkov@gmail.com>
2019-12-29 12:41:53 +0200

    login form hadle submit

--- next-cookie-auth/server.js
+++ next-cookie-auth/server.js
@@ -1,20 +1,28 @@
-const next =require('next')
-const express =require('express');
+const next = require("next");
+const express = require("express");
 
-const dev =process.env.NODE_ENV!=='production';
-const port =process.env.port||3000;
-const app=next({dev});
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.port || 3000;
+const app = next({ dev });
 
-const handler=app.getRequestHandler()
+const handler = app.getRequestHandler();
 
-app.prepare().then(()=>{
-    const server=express();
-    server.get('*',(req,res)=>handler(req,res))
-    server.listen(port,err=>{
-        if(err){
-            console.log(err.message)
-            throw err
-        }
-        console.log(`Listen on ${port}`)
-    })
-})
+app.prepare().then(() => {
+  const server = express();
+
+  //parsing json in express like bodyparser
+  server.use(express.json());
+
+  server.post("/api/login", (req, res) => {
+    const { email, password } = req.body;
+    res.json({ email, password, success: true });
+  });
+  server.get("*", (req, res) => handler(req, res));
+  server.listen(port, err => {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    console.log(`Listen on ${port}`);
+  });
+});
